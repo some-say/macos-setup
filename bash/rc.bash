b008544dc8cddaa5bffd268ca0c1ddf143034181
Rômulo Alves <github@romuloalv.es>
2017-05-26 17:59:14 -0300

    Adding my bash file

--- bash/rc.bash
+++ bash/rc.bash
@@ -1,20 +1,4 @@
-export PS1="\[\e[32m\]\u\[\e[m\]@\[\e[33m\]\h\[\e[m\]:\[\e[36m\]\w\[\e[m\] \[\e[33m\]\\$\[\e[m\] "
+alias gvim='/Applications/MacVim.app/Contents/MacOS/Vim -g'
 
-fasd_cd() {
-  if [ $# -le 1 ]; then
-    fasd "$@"
-  else
-    local _fasd_ret="$(fasd -e echo "$@")"
-    [ -z "$_fasd_ret" ] && return
-    [ -d "$_fasd_ret" ] && cd "$_fasd_ret" || echo "$_fasd_ret"
-  fi
-}
-
-alias g=git
-source ~/.zsh/aliases.zsh
-source ~/.nurc
-
-source /usr/local/opt/chruby/share/chruby/chruby.sh
-source /usr/local/opt/chruby/share/chruby/auto.sh
-
-chruby 2
+alias ni='npm install'
+alias nig='npm install --global'
